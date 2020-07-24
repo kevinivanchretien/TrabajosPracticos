70848f2ce5669beb32f0bd75f1a58d60dd9faaf1
Kevin ivan Chretien <68036359+kevinivanchretien@users.noreply.github.com>
2020-07-23 22:40:36 -0300

    Add files via upload

---
+++ jsEntradaSalida-02.js
@@ -0,0 +1,11 @@
+/*
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	//declarar una variable
+	var vnombre;
+	vnombre = prompt("ingresar nombre","nombre");
+	alert(vnombre);
+}
+
---
+++ jsEntradaSalida-03.js
@@ -0,0 +1,20 @@
+/*
+Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var ojo;
+
+	ojo = document.getElementById("txtIdNombre").value;
+
+    alert(ojo);
+
+
+	/*var nombreIngresado;
+	nombreIngresado =(txtIdNombre.value);
+	
+	alert( nombreIngresado );*/
+
+}
+
+
